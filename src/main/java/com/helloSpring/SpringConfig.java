5d485a2e33f8f28a8c15a7d60dfa547956486758
정준영 <jyjeong620@naver.com>
2020-10-05 14:42:36 +0900

    회원 가입기능 회원목록 조회기능

--- src/main/java/com/helloSpring/SpringConfig.java
+++ src/main/java/com/helloSpring/SpringConfig.java
@@ -9,16 +9,16 @@ import org.springframework.context.annotation.Configuration;
 /**
  * 자바코드로 직접 스프링 빈 등록하는 방법
  */
-@Configuration
-public class SpringConfig {
-
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
-}
+//@Configuration
+//public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+//}
--- src/main/java/com/helloSpring/controller/MemberController.java
+++ src/main/java/com/helloSpring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.helloSpring.controller;
 
+import com.helloSpring.domain.Member;
 import com.helloSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,35 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    /**
+     * 회원가입 버튼을 눌렀을 경우 뷰리졸브가 members/createMemberForm를 호출
+     * @return
+     */
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    /**
+     * members/createMemberForm 화면에서 input으로 텍스트를 입력받았을때 post방식으로 호출하는 메소드
+     * @return
+     */
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member= new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
