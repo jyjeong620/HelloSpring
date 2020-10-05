ff02cea121348a8c32075746ad574914be910eaf
정준영 <jyjeong620@naver.com>
2020-10-05 10:35:05 +0900

    자바코드로 직접 Spring Bean에 등록

---
+++ src/main/java/com/helloSpring/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.helloSpring;
+
+import com.helloSpring.repository.MemberRepository;
+import com.helloSpring.repository.MemoryMemberRepository;
+import com.helloSpring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 자바코드로 직접 스프링 빈 등록하는 방법
+ */
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
