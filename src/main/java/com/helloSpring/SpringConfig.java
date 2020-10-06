e5a168c7f6dd7becd04a55face870521d18c0f29
정준영 <jyjeong620@naver.com>
2020-10-06 16:20:58 +0900

    JDBC를 이용한 Service

--- src/main/java/com/helloSpring/SpringConfig.java
+++ src/main/java/com/helloSpring/SpringConfig.java
@@ -1,24 +1,36 @@
 package com.helloSpring;
 
+import com.helloSpring.repository.JdbcMemberRepository;
 import com.helloSpring.repository.MemberRepository;
 import com.helloSpring.repository.MemoryMemberRepository;
 import com.helloSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 자바코드로 직접 스프링 빈 등록하는 방법
  */
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-//    }
-//}
+        return new JdbcMemberRepository(dataSource);
+    }
+}
